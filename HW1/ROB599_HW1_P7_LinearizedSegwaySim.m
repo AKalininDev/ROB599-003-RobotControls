ae6ce4d073f0a2fdcf375588841a6c6a5cd2b80e
TonyxxA <anatoly.k2000@gmail.com>
2024-09-08 17:04:31 -0400

    Linearized segway model simulation

---
+++ HW1/ROB599_HW1_P7_LinearizedSegwaySim.m
@@ -0,0 +1,109 @@
+clear
+clc
+close all
+
+%% System Parameters
+
+param.m = 0.2;
+param.M = 0.5;
+param.J = 0.006;
+param.l = 0.3;
+param.c = 0.1;
+param.gamma = 0.1;
+param.g = 9.81;
+
+
+%% Simulation Parameters
+to = 0;
+tFinal = 10;
+tSim = [to tFinal];
+xo = [0; 0; pi/8; 0];
+
+[tOut, xOut] = ode45(@(t,x) segwayLinearized(t, x, param), tSim, xo);
+
+
+% Calculate the applied force F(t) at each time step
+F_t = zeros(length(tOut), 1);
+for i = 1:length(tOut)
+    F_t(i) = calculate_force(tOut(i));
+end
+
+%% Show the Plots
+figure('Position', [100, 100, 800, 1000]); % Increase figure size (width x height)
+
+sgtitle('Segway System Dynamics Simulation. Variable External Force.', 'FontSize', 24, 'FontWeight', 'bold');
+
+% Plot Cart Position
+subplot(5,1,1);
+plot(tOut, xOut(:,1), 'LineWidth', 3, 'Color', 'b');
+ylabel('Cart Pose (m)', 'FontSize', 12, 'FontWeight', 'bold');
+set(gca, 'FontSize', 12); % Increase font size of x and y ticks
+grid on;
+
+% Plot Cart Velocity
+subplot(5,1,2);
+plot(tOut, xOut(:,2), 'LineWidth', 3, 'Color', 'g');
+ylabel('Cart Vel (m/s)', 'FontSize', 12, 'FontWeight', 'bold');
+set(gca, 'FontSize', 12); % Increase font size of x and y ticks
+grid on;
+
+% Plot Pendulum Angle
+subplot(5,1,3);
+plot(tOut, xOut(:,3), 'LineWidth', 3,'Color', [1 0.5 0]);
+ylabel('Pend. Angle (rad)', 'FontSize', 12, 'FontWeight', 'bold');
+set(gca, 'FontSize', 12); % Increase font size of x and y ticks
+grid on;
+
+% Plot Pendulum Angular Velocity
+subplot(5,1,4);
+plot(tOut, xOut(:,4),  'LineWidth', 3, 'Color', 'm');
+ylabel('Pendulum Ang. Vel. (rad/s)', 'FontSize', 12, 'FontWeight', 'bold');
+set(gca, 'FontSize', 12); % Increase font size of x and y ticks
+grid on;
+
+% Plot Applied Force
+subplot(5,1,5);
+plot(tOut, F_t, 'LineWidth', 3, 'Color', 'r');
+xlabel('Time (s)', 'FontSize', 14, 'FontWeight', 'bold');
+ylabel('Force (N)', 'FontSize', 12, 'FontWeight', 'bold');
+set(gca, 'FontSize', 12); % Increase font size of x and y ticks
+grid on;
+
+% Adjust spacing between subplots to prevent overlap
+set(gcf, 'Position', [100, 100, 800, 1000]); % Adjust figure size to increase screen area
+sgtitle('Segway System Dynamics Simulation. Variable External Force Over Time.', 'FontSize', 24, 'FontWeight', 'bold');
+
+
+%% Calculate the Force Applied to the Cart
+function F = calculate_force(t)
+    if (t < 0)
+        F = 0;
+    elseif (t <= 2.5)
+        F = 0.1 * cos(10 * t);
+    elseif (t < 5)
+        F = 0.5 * cos(5 * t);
+    else
+        F = 1.0 * cos(5 * t);
+    end
+end
+
+%% State Space Of the System
+function xdot = segwayLinearized(t, x, param)
+
+    % State variables
+    x1 = x(1); % Cart position
+    x2 = x(2); % Cart velocity
+    x3 = x(3); % Pendulum angle
+    x4 = x(4); % Pendulum angular velocity
+
+    F = calculate_force(t);
+    
+    
+    x1dot = x2;
+    x2dot = ((param.J + param.m*param.l^2)*param.c)/(param.l^2*param.m^2 - (param.J + param.m*param.l^2)*(param.M+param.m)) * x2 -(param.g*param.l^2*param.m^2)/(param.l^2*param.m^2 - (param.J + param.m*param.l^2)*(param.M+param.m)) * x3 + (param.gamma*param.l*param.m)/(param.l^2*param.m^2 - (param.J + param.m*param.l^2)*(param.M+param.m))*x4 - (param.J + param.m*param.l^2)/(param.l^2*param.m^2 - (param.J + param.m*param.l^2)*(param.M+param.m)) * F ;
+    x3dot = x4;
+    x4dot = (param.c*param.l*param.m)/(param.l^2*param.m^2 - (param.J + param.m*param.l^2)*(param.M+param.m)) * x2 -((param.M+param.m)*param.g*param.l*param.m)/(param.l^2*param.m^2 - (param.J + param.m*param.l^2)*(param.M+param.m)) * x3 + ((param.M+param.m)*param.gamma)/(param.l^2*param.m^2 - (param.J + param.m*param.l^2)*(param.M+param.m))*x4 - (param.m*param.l)/(param.l^2*param.m^2 - (param.J + param.m*param.l^2)*(param.M+param.m)) * F;
+
+    xdot = [x1dot; x2dot; x3dot; x4dot];
+
+end
