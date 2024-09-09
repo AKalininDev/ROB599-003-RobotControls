c827a9cdb8d921f2450ccaad87aaaadad4836e9b
TonyxxA <anatoly.k2000@gmail.com>
2024-09-08 21:52:01 -0400

    separated problems into separate files

--- HW1/ROB599_HW1_P7_LinearizedSegwayStability.m
+++ HW1/ROB599_HW1_P7_LinearizedSegwayStability.m
@@ -7,11 +7,6 @@ syms c gamma
 syms g
 
 
-% Substitutions
-
-syms Jt Mt
-
-
 x1_eq = 0;
 x2_eq = 0;
 x3_eq = 0;
@@ -25,15 +20,6 @@ x1dot = x2;
 x2dot = (F*(J + m*l^2) - (J + m*l^2)*c*x2 - (J + m*l^2)*l*m*x4^2*sin(x3) + g*l^2*m^2*sin(2*x3)/2 - gamma*l*m*x4*cos(x3))/((J + m*l^2)*(M+m) - l^2*m^2*cos(x3)*cos(x3));
 x3dot = x4;
 x4dot = (F*l*m*cos(x3) + (M+m)*g*l*m*sin(x3) - (M+m)*gamma*x4 - c*l*m*x2*cos(x3) - l^2*m^2*x4^2*sin(2*x3)/2)/((J + m*l^2)*(M+m) - l^2*m^2*cos(x3)*cos(x3));
-
-
-% Subs for simplifications
-x2dot = subs(x2dot, (J + m*l^2), Jt);
-x2dot = subs(x2dot, (M + m), Mt);
-x4dot = subs(x4dot, (J + m*l^2), Jt);
-x4dot = subs(x4dot, (M + m), Mt);
-
-
 
 xdot = [x1dot x2dot x3dot x4dot];
 x = [x1; x2; x3; x4];
@@ -49,3 +35,22 @@ A = subs(A, u, u_eq)
 
 B = subs(jacu, u, u_eq);
 B = subs(B, x, x_eq)
+
+%% Evaluating A, B with given parameters
+
+param.m = 0.2;
+param.M = 0.5;
+param.J = 0.006;
+param.l = 0.3;
+param.c = 0.1;
+param.gamma = 0.1;
+param.g = 9.81;
+
+
+A_eval = eval(subs(A, {m, M, J, l, c, gamma, g}, {param.m, param.M, param.J, param.l, param.c, param.gamma, param.g}))
+B_eval = eval(subs(B, {m, M, J, l, c, gamma, g}, {param.m, param.M, param.J, param.l, param.c, param.gamma, param.g}))
+
+
+%% Compute Eigenvalues
+
+[V, D] = eig(A_eval)
