b5bb7e8f892b10dfb49c572be22bc7f4d966c630
Anatolii Kalinin <anatoly.k2000@gmail.com>
2024-10-04 10:25:45 -0400

    polishing the model

--- HW2/ROB599_HW2_P3_A.m
+++ HW2/ROB599_HW2_P3_A.m
@@ -1,5 +1,5 @@
 %% ROB599-HW2-Problem-3A
-% Simulating inverted pendulum controller using a 
+% Simulating inverted pendulum controller using a
 % linearised model (linearized only @equillibrium x1=0, x2=0.
 %% Cleanup
 clear
@@ -8,7 +8,7 @@ close all
 
 %% System Parameters (SI)
 
-% Intertial
+% Inertial
 param.m = 0.2;      %kg
 param.J = 0.006;    %kgm2
 param.l = 0.3;      %m
@@ -19,7 +19,7 @@ param.gamma = 0.1; %N*m*sec/rad
 % Fundamental
 param.g = 9.81;    %N/kg
 
-% Disturmabces
+% Disturbances
 param.w = 3;       %rad/sec
 
 % Controller Gains
@@ -38,22 +38,19 @@ a_2 = [(1/(param.J + param.m * param.l^2)) * ((param.m*param.g - param.kp)*param
 
 A = [a_1; a_2]
 
-
 % Input Matrix
-b_1 = 0; % assuming no disturbance
-b_2 = (1/(param.J + param.m * param.l^2)) * param.kp*param.l;
+b_1 = [0, 0];
+b_2 = [(param.l/(param.J + param.m * param.l^2)) * param.kp, (param.l/(param.J + param.m * param.l^2)) * param.kd];
 
 B = [b_1; b_2]
 
 %% Checking Stability of the System
 
 [V, D] = eig(A);
-D 
-
-
+D
 %% Solve for x(t->inf) (Stable Linearized System)
 X_final = -inv(A) * B
-X_final_eval = X_final*systemInput()
+X_final_eval = X_final*[systemInput(); param.w]
 
 %% Simulation Parameters
 to = 0;
@@ -77,38 +74,40 @@ figure('Position', [100, 100, 1200, 900]);  % Width: 1200, Height: 900
 
 sgtitle('Figure 1. Linearized Inverted Pendulum. Feedback Controller. Kp = 5, Kd = 0.1',...
     'FontSize', 24, 'FontWeight', 'bold');
- 
+
 % Plot Pendulum Angle Command Signal    
 subplot(3,1,1);
-plot(tSim, theta_command, 'LineWidth', 3, 'Color', 'r');
-ylabel('\fontsize{14}{16}\textbf{Input $\theta_{\mathrm{cmd}}$ (rad)}', 'FontSize', 20, ...
+plot(tSim, theta_command, 'LineWidth', 2.5, 'Color', '#0072BD');
+ylabel('\fontsize{14}{16}\textbf{Input $\theta_{\mathrm{cmd}}$ (rad)}', 'FontSize', 18, ...
     'FontWeight', 'bold', 'Interpreter', 'latex');
-set(gca, 'FontSize', 12); 
+set(gca, 'FontSize', 14, 'Box', 'off', 'LineWidth', 1.5, 'FontName', 'Helvetica');
 grid on;
-
+grid minor;
 
 % Plot Pendulum Angular Position
 subplot(3,1,2);
-plot(tOut, xOut(:,1), 'LineWidth', 3, 'Color', 'b');
-ylabel('\fontsize{14}{16}\textbf{x1 = ${\theta}$ (rad)}', 'FontSize', 20, ...
+plot(tOut, xOut(:,1), 'LineWidth', 2.5, 'Color', '#D95319');
+ylabel('\fontsize{14}{16}\textbf{x1 = ${\theta}$ (rad)}', 'FontSize', 18, ...
     'FontWeight', 'bold', 'Interpreter', 'latex');
-set(gca, 'FontSize', 12);
+set(gca, 'FontSize', 14, 'Box', 'off', 'LineWidth', 1.5, 'FontName', 'Helvetica');
 grid on;
+grid minor;
 
 % Plot Pendulum Angular Velocity
 subplot(3,1,3);
-plot(tOut, xOut(:,2), 'LineWidth', 3, 'Color', 'g');
-ylabel('\fontsize{14}{16}\textbf{x2 = $\dot{\theta}$ (rad/s)}', 'FontSize', 20, ...
+plot(tOut, xOut(:,2), 'LineWidth', 2.5, 'Color', '#EDB120');
+ylabel('\fontsize{14}{16}\textbf{x2 = $\dot{\theta}$ (rad/s)}', 'FontSize', 18, ...
     'FontWeight', 'bold', 'Interpreter', 'latex');
-set(gca, 'FontSize', 12); 
-xlabel('Time (s)', 'FontSize', 16, 'FontWeight', 'bold');
+set(gca, 'FontSize', 14, 'Box', 'off', 'LineWidth', 1.5, 'FontName', 'Helvetica');
+xlabel('Time (s)', 'FontSize', 18, 'FontWeight', 'bold', 'FontName', 'Helvetica');
 grid on;
+grid minor;
 
 % Reduce Margins
 set(gca, 'LooseInset', max(get(gca, 'TightInset'), 0.02));
 
 print(gcf, 'ROB599-HW#2-Problem_3A-Inverted_Pendulum_PD_Control.png', ...
-    '-dpng', '-r300'); 
+    '-dpng', '-r300');
 
 
 %% Target Angle Calculator
@@ -123,11 +122,11 @@ function xdot = LinearizedInvertedPendulumFB(t, x, param)
     x1 = x(1);
     x2 = x(2);
     theta_command = systemInput(t);
-   
-    x1dot = x2 + param.w;
-    x2dot = (1/(param.J + param.m * param.l^2))* ((param.m*param.g - param.kp)*param.l * x1 ...
+
+    x1dot = x2;
+    x2dot = (1/(param.J + param.m * param.l^2)) * ((param.m*param.g - param.kp)*param.l * x1 ...
         - (param.gamma + param.l*param.kd)*x2 ...
-        + param.kp*param.l*theta_command);
+        + param.kp*param.l*theta_command + param.l*param.kd*param.w);
     
     xdot = [x1dot; x2dot];
 
