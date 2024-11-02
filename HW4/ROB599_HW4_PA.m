2b32b63ed13a4168754da2b4663cb34cdc34d08c
Anatolii Kalinin <anatoly.k2000@gmail.com>
2024-11-02 15:40:50 -0400

    Completed Problem 2, Part A

--- HW4/ROB599_HW4_PA.m
+++ HW4/ROB599_HW4_PA.m
@@ -1,11 +1,21 @@
-%% ROB599_HW4_P1_A
+%% ROB599_HW4_PA
 % Simiulating a PD Controller with Motor Angle Feedback for Series Elastic Actuator.
+% Problem 1A: Derive the state space representation of the system.
+% Problem 2A: Add numerical values of the parameters to the system and determine kd range such that the system is stable using Routh Hurwitz.
+% Problem 3A: Determine the transfer function of the system.
+% Problem 4A: Create root locus plot for the system.
 
 %% Clean Up
 clear
 clc
 
-%% Define System Variables
+%%
+% <html><h2> Problem 1A </h2></html>
+%
+% Deriving the State Space Representation of the System.
+
+%%
+% <html><h3> Define System Variables </h3></html>
 
 % Inertia Terms
 syms Jm Jl
@@ -24,13 +34,14 @@ syms u
 % Control Terms
 syms kp kd theta_cmd
 
-
-%% Define EOMs
+%%
+% <html><h3> Define EOMs </h3></html>
 eq1 = Jm*theta_m_ddot + Bm*theta_m_dot - k*(theta_l - theta_m) == u
 eq2 = Jl*theta_l_ddot + Bl*theta_l_dot + k*(theta_l - theta_m) == 0
 eq3 = u == kp*(theta_cmd - theta_m) - kd*theta_m_dot
 
-%% Define State Space
+%%
+% <html><h3> Acquire State Space Representaiton </h3></html>
 
 % State Space Variables
 syms x1 x2 x3 x4
@@ -46,12 +57,12 @@ u_state = solve(u_state, u);
 eq1_state = subs(eq1_state, u, u_state);
 x2_dot = solve(eq1_state, x2_dot);
 x2_dot = simplify(x2_dot);
-x2_dot = collect(x2_dot, [x1, x2, x3, x4, theta_cmd])
+x2_dot = collect(x2_dot, [x1, x2, x3, x4, theta_cmd]);
 
 % Solve for x4_dot
 x4_dot = solve(eq2_state, x4_dot);
 x4_dot = simplify(x4_dot);
-x4_dot = collect(x4_dot, [x1, x2, x3, x4, theta_cmd])
+x4_dot = collect(x4_dot, [x1, x2, x3, x4, theta_cmd]);
 
 % Form state equations
 x_dot = [x2;
@@ -65,3 +76,79 @@ A = simplify(A)
 
 B = jacobian(x_dot, theta_cmd);
 B = simplify(B)
+
+%%
+% <html><h2> Problem 2 A </h2></html>
+%
+% Determine the range of kd such that the system is stable using Routh Hurwitz.
+
+% Adding numerical values of the parameters
+param.Jm = 0.0097;
+param.Jl = 0.0097;
+
+param.Bm = 0.04169;
+param.Bl = 0.04169;
+
+param.k = 100;
+
+param.kp = 10;
+
+A_eval = subs(A, [Jm, Jl, Bm, Bl, k, kp], [param.Jm, param.Jl, param.Bm, param.Bl, param.k, param.kp]);
+B_eval = subs(B, [Jm, Jl, Bm, Bl, k, kp], [param.Jm, param.Jl, param.Bm, param.Bl, param.k, param.kp]);
+
+A_num = vpa(A_eval, 3)
+B_num = vpa(B_eval, 3)
+
+%%
+% <html><h3> Acquire Characteristic Equations </h3></html>
+% Characteristic Equation: det(A - lambda*I) = 0
+
+syms lambda
+char_eq = det(A_eval - lambda*eye(4));
+char_eq = collect(char_eq, lambda);
+char_eq_num = vpa(char_eq, 3)
+
+%%
+% <html><h3> Construct Routh-Hurwitz Matrix </h3></html>
+
+routh_matrix = getRouthMatrix(char_eq, lambda);
+routh_matrix_num = vpa(routh_matrix, 3)
+
+%%
+% <html><h3> Compute Principle Minors of Routh-Hurwitz Matrix </h3></html>
+
+principalMinors = computePrincipalMinors(routh_matrix);
+showFactoredForm(principalMinors, kd);
+
+%%
+% <html><h3> Solve for Stability Requirements </h3></html>
+
+%inequalities = sym([]);
+inequalities = [0 < kd];
+for i=1:length(principalMinors)
+    inequality = principalMinors(i) > 0;
+    inequalities = [inequalities, inequality];
+end
+
+inequalitiesSol = solveRealStrictInequalities(inequalities, kd);
+inequalities_sol = vpa(inequalitiesSol, 3)
+
+
+%%
+% <html><h2> Helper Functions </h2></html>
+%
+
+% Display the factored form of the expression
+function showFactoredForm(expressions, var)
+factoredForm = sym([]);
+for i=1:length(expressions)
+    factoredForm = [factoredForm, prod(factor(expressions(i), var, "FactorMode", "real"))];
+end
+
+for i=1:length(factoredForm)
+    
+    % Display the factored form of the expression
+    factoredFormEquation = vpa(factoredForm(i), 3)
+end
+
+end
